0d62ba595f1fb339e5664d0a4f0dbe1b0407a009
TimFarrR <ftr-neft@mail.ru>
2024-08-06 12:58:58 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,28 @@
+import sys
+
+def generate_circular_array(n):
+    return list(range(1, n + 1))
+
+def find_path(n, m):
+    circular_array = generate_circular_array(n)
+    path = []
+    current_index = 0
+
+    while True:
+        path.append(circular_array[current_index])
+        current_index = (current_index + m - 1) % n
+        if current_index == 0:
+            break
+
+    return path
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <n> <m>")
+        sys.exit(1)
+    
+    n = int(sys.argv[1])
+    m = int(sys.argv[2])
+
+    path = find_path(n, m)
+    print("".join(map(str, path)))
